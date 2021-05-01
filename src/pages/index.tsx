446ac38d2b50383ec95e473ee9e077eceef9b8e8
yuiseki <yuiseki@gmail.com>
2021-05-01 18:11:03 +0900

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+/// <reference types="@emotion/react/types/css-prop" />
+import tw, { css } from "twin.macro";
+
+const container = css`
+  ${tw`mx-auto m-4 p-4 rounded bg-gray-400`}
+`;
+
+export const Home = (): JSX.Element => (
+  <div css={container}>
+    <main>
+      <h1 tw="text-5xl font-bold">
+        Nextjs App with TypeScript, ESlint, Jest, Emotion, Tailwind and Twin
+      </h1>
+    </main>
+  </div>
+)
+
+export default Home
