a4ea6a4d79495c600c0fe5d85f08a09bd29ffee7
silloi <silloi1993@gmail.com>
2021-05-22 15:55:10 +0900

    chore: css やめる

--- src/pages/network.tsx
+++ src/pages/network.tsx
@@ -37,36 +37,19 @@ const NetworkDiagram = () => {
   return <Graph graph={graph} />;
 };
 
-const container = css`
-  height: 98%;
-  display: flex;
-  flex-direction: column;
-  ${tw`m-4 p-4 rounded bg-gray-600`}
-`;
-
-const headerContainer = css`
-  height: 15em;
-  width: 100%;
-`;
-
-const innerContainer = css`
-  height: 100%;
-  width: 100%;
-`;
-
 const Network = () => {
   return (
     <>
       <Head>
         <title>貧困可視化プロトタイプ：ネットワーク</title>
       </Head>
-      <div css={container}>
-        <div css={headerContainer}>
+      <div tw="h-full flex flex-col m-4 p-4 rounded bg-gray-600">
+        <div tw="h-64 w-full">
           <h1 tw='text-5xl text-white font-bold'>
             貧困可視化プロトタイプ：ネットワーク
           </h1>
         </div>
-        <div css={innerContainer}>
+        <div tw="h-full w-full">
           <NetworkDiagram />
         </div>
       </div>
