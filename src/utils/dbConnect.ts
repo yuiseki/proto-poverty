307b0015bd76c0a9ef3629505fec2b5f7fdfd76d
ao-spirogyra <lily.spirogyra@gmail.com>
2021-05-22 16:44:30 +0900

    これで動く

--- src/utils/dbConnect.ts
+++ src/utils/dbConnect.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI ?? 'mongodb://localhost:27017';
 
 if (!MONGODB_URI) {
   throw new Error(
