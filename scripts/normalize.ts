b8e615f7de3ea0445c9ff5957067b952b909d449
yuiseki <yuiseki@gmail.com>
2021-05-30 14:47:03 +0900

    NFKDではなくNFKCにするべきらしい

--- scripts/normalize.ts
+++ scripts/normalize.ts
@@ -6,11 +6,11 @@ import { transform } from '@moneyforward/stream-util';
 program.parse(process.argv);
 if (program.args.length) {
   const text = program.args[0];
-  console.log(text.normalize('NFKD').replaceAll(' ', ''));
+  console.log(text.normalize('NFKC').replaceAll(' ', ''));
 } else {
   (async () => {
     for await (const line of process.stdin.pipe(new transform.Lines())) {
-      console.log(line.normalize('NFKD').replaceAll(' ', ''));
+      console.log(line.normalize('NFKC').replaceAll(' ', ''));
     }
   })();
 }
